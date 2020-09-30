d676a99ed55372f82418f69406f5fbda06daacc0
James Min <jmin499@gmail.com>
2020-09-30 18:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var cart = [];
 
-function getCart(item) {
- cart.push( {itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1} )
- return `${item} has been added to your cart.`;
+function getCart() {
+ return cart
 }
 
 function setCart(c) {
@@ -12,6 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push( {itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1} )
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
